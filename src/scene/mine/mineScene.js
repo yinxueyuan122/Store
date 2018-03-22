098e6257e31c308373d2dcaa34ab40a798cbf476
cn40798 <yin.xueyuan@navercorp.com>
2018-03-22 19:50:36 +0800

    test

--- src/scene/mine/mineScene.js
+++ src/scene/mine/mineScene.js
@@ -9,7 +9,7 @@ export default class MineScene extends Component {
       <View style={{padding: 10}}>
        
         <Text style={{padding: 10, fontSize: 42}}>
-          MineScene
+          MineScene2
         </Text>
       </View>
     );
